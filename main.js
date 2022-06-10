2bd5c75831965c5d4004ce990a6ffa1d81736227
DoranL <ljhstarting414@gmail.com>
2022-06-10 16:39:16 +0900

    mainchange

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-let mainText = document.querySelector("h1");
+// let mainText = document.querySelector("h1");
+// let nextPageText = document.querySelector("h4");
 
-window.addEventListener("scroll", function(){
-    let value = window.scrollY;
+// window.addEventListener("scroll", function(){
+//     let value = window.scrollY;
     
 
-    if(value>300){
-        mainText.style.animation = "slideOut 2s ease-out forwards";
+//     if(value>1){
+//         mainText.style.animation = "slideOut 2s ease-out forwards";
 
-    }
+//     }
 
-    else{
-        mainText.style.animation="slideIn 2s ease-out";
-    }
-});
+//     else{
+//         mainText.style.animation="slideIn 2s ease-out";
+//     }
+// });
